5129ebbf7643a9b26e96be1eb6492843e297c636
Rafail Jabrayilov <romasecure84@yandex.ru>
2024-04-14 00:20:22 +0400

    implicityWait metodu islendi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,13 @@
 package org.example;
 
+import dev.failsafe.internal.util.Assert;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -10,10 +15,9 @@ public class Main {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         WebDriver driver=new ChromeDriver();
-
-        driver.get("https://www.google.com");
-        System.out.println(driver.getTitle());
-        System.out.println(driver.getCurrentUrl());
+        driver.manage().window().setPosition(new Point(100,100));
+        driver.manage().window().setSize(new Dimension(200,400));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.close();
     }
 }
---
+++ src/test/java/Test01.java
@@ -0,0 +1,7 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test01 {
+
+}
