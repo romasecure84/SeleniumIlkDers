7350d27706f39e17b78936cb1d3d5c10290b25d3
Rafail Jabrayilov <romasecure84@yandex.ru>
2024-04-14 00:49:33 +0400

    yeni test dersi elave edildi

--- src/test/java/Test01.java
+++ src/test/java/Test01.java
@@ -1,7 +1,23 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Test01 {
+import java.time.Duration;
 
+public class Test01 {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1382, 744));
+        driver.manage().window().setPosition(new Point(1912, 303));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://www.amazon.com.tr");
+        driver.findElement(By.xpath("//*[@id='sp-cc-rejectall-link']")).click();
+        System.out.println(driver.manage().window().getPosition());
+        System.out.println(driver.manage().window().getSize());
+        driver.quit();
+    }
 }
